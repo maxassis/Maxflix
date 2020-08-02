4d2474980fecd9e6b47e72faf604d4651334c0ff
maxwelb-assis <max.assis@ymail.com>
2020-08-02 14:18:19 -0300

    iniciando o heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+<<<<<<< Updated upstream
+=======
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+>>>>>>> Stashed changes
 
 function CadastroCategoria() {
   return (
     <PageDefault>
+<<<<<<< Updated upstream
       <h1>Cadastro de Categoria</h1>
+=======
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria"
+          type="textarea"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+>>>>>>> Stashed changes
 
       <form>
 
@@ -16,9 +48,13 @@ function CadastroCategoria() {
           />
         </label>
 
+<<<<<<< Updated upstream
         <button>
+=======
+        <Button> 
+>>>>>>> Stashed changes
           Cadastrar
-        </button>
+        </Button>
       </form>
 
 
